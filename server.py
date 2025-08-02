a84ef1a84281800ce79ac00dcf4fbb992b72c4b3
Kelvin Lee <129922096+Kelvin-Lee-20@users.noreply.github.com>
2025-08-03 00:01:20 +0800

    add image classification api

--- server.py
+++ server.py
@@ -7,6 +7,20 @@ from flask_cors import CORS
 from sklearn.cluster import KMeans 
 from transformers import pipeline
 from flask import request
+import transformers
+import ssl
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import keras
+from keras.applications.resnet50 import ResNet50
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+from tensorflow.keras.utils import get_file
+from tensorflow.keras.preprocessing import image
+import random
+
+ssl._create_default_https_context = ssl._create_unverified_context
+model = ResNet50(weights='imagenet')
 
 def load_iris_data():
     iris = load_iris()
@@ -39,7 +53,6 @@ def sa():
     text = request.args.get('text')
     if text is None or text.strip() == '':
         return jsonify({"error": "Resource not found"}), 404
-
     classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
     result = classifier(text)
     data = {
@@ -47,6 +60,39 @@ def sa():
         'score': result[0]['score']
     }
     return jsonify(data)
+
+@app.route('/api/image-classification', methods=['POST'])
+def imageClassification():
+    if 'file' not in request.files:
+        return jsonify({"error": "Resource not found"}), 404
+    file = request.files['file']
+    if file:
+        try:
+            model = ResNet50(weights='imagenet')
+            img_bytes = file.read()
+            img_stream = BytesIO(img_bytes)
+            img = Image.open(img_stream)
+            if img.mode != 'RGB':
+                img = img.convert('RGB')
+            img = img.resize((224, 224))
+
+            # img_path = 'https://live.staticflickr.com/41/114587853_cdabf6568c_o.jpg'
+            # img_path = 'http://images.cocodataset.org/train2017/000000391898.jpg'
+            # local_path = get_file(origin=img_path)
+            # img = image.load_img(local_path, color_mode='rgb', target_size=(224, 224))
+
+            x = image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            x = preprocess_input(x)
+            preds = model.predict(x)
+            predictions = decode_predictions(preds, top=3)[0]
+            result = [
+                {'label': pred[1], 'probability': float(pred[2])} 
+                for pred in predictions
+            ]
+            return jsonify({'predictions': result})
+        except Exception as e:
+            return jsonify({"error": "Resource not found"}), 404
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
